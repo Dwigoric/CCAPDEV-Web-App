e83163915c4a90977cbe566ee3b3cd2202fefff0
Joshua Permito <dwigoric@insanedevs.com>
2023-06-12 20:22:34 +0800

    refactor: compartmentalize login form and login box

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import LoginForm from './LoginForm.vue'
+</script>
 
 <template>
     <div id="loginBox">
@@ -6,28 +8,10 @@
             Welcome to
             <span id="title">Compact Donuts</span>
         </div>
-        <div id="loginForm">
-            <input
-                class="inputField"
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Username"
-            />
-            <input
-                class="inputField"
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-            />
-            <div id="loginButton">
-                <button type="submit">Login</button>
-            </div>
-        </div>
+        <LoginForm />
         <div id="signUpPrompt">
             Don't have an account?
-            <a>Sign up</a>
+            <RouterLink to="/register">Sign up</RouterLink>
         </div>
     </div>
 </template>
@@ -58,53 +42,6 @@
     display: block;
     font-size: 2.5rem;
     font-weight: bold;
-}
-
-#loginForm {
-    display: flex;
-    flex-flow: column wrap;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-}
-
-.inputField {
-    margin: 10px;
-    flex-basis: 75%;
-    min-width: 75%;
-    float: left;
-}
-
-button {
-    font-family: 'Source Sans 3', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-        Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 1.2rem;
-    font-weight: 500;
-    color: var(--vt-c-black-soft);
-    background-color: var(--color-dark-pink);
-    border: none;
-    border-radius: 20px;
-    padding: 15px 25px;
-    margin: 8px 0;
-    cursor: pointer;
-    width: 100%;
-}
-
-button:hover {
-    background-color: var(--color-bright-pink);
-}
-
-input[type='text'],
-input[type='password'] {
-    font-family: 'Source Sans 3', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-        Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    width: 100%;
-    padding: 20px 25px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid var(--vt-c-black-soft);
-    border-radius: 30px;
-    background-color: var(--vt-c-white-soft);
 }
 
 @media (min-width: 1024px) {
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,72 @@
+<script setup></script>
+
+<template>
+    <div id="loginForm">
+        <input
+            class="inputField"
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+        />
+        <input
+            class="inputField"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+        />
+        <div id="loginButton">
+            <button type="submit">Login</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#loginForm {
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+
+.inputField {
+    margin: 10px;
+    flex-basis: 75%;
+    min-width: 75%;
+    float: left;
+}
+
+button {
+    font-family: 'Source Sans 3', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: var(--vt-c-black-soft);
+    background-color: var(--color-dark-pink);
+    border: none;
+    border-radius: 20px;
+    padding: 15px 25px;
+    margin: 8px 0;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    background-color: var(--color-bright-pink);
+}
+
+input[type='text'],
+input[type='password'] {
+    font-family: 'Source Sans 3', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    width: 100%;
+    padding: 20px 25px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid var(--vt-c-black-soft);
+    border-radius: 30px;
+    background-color: var(--vt-c-white-soft);
+}
+</style>
