8affbc0857a5ab64807f062e877891c14afaece7
ProfRedacted <king.uy234@gmail.com>
2023-06-18 22:41:12 +0800

    Added the post layout
    Added the CCS and the images for the first post within the website

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -10,8 +10,14 @@ document.title = 'Compact Donuts | Feed'
     <div id="feed">
         <div class="feed-element" id="left-sidebar">This is the left sidebar.</div>
         <div class="feed-element" id="posts">
-            <span>This is where the posts go.</span>
-            <span>There are no posts yet.</span>
+            <div class="flexboxColumn" id="PostLayout">
+                <div class="flexboxRow">
+                    <span class="profilePic" id="adachiPfp"></span>
+                    <span class="originalPoster"> Adachi &nbsp </span>
+                    <span class="originalPoster"> @Cabbage_Man </span>
+                </div>
+                <div class="postContent" id="adachiPost"></div>
+            </div> 
         </div>
         <div class="feed-element" id="right-sidebar">
             This is the right sidebar.
@@ -60,6 +66,55 @@ document.title = 'Compact Donuts | Feed'
     flex-flow: row nowrap;
     justify-content: left;
     flex-grow: 3;
+}
+/* Flex Box for the class*/
+.flexboxColumn {
+     display: flex;
+     flex-direction: column;
+}
+
+.flexboxRow {
+     display: flex;
+     flex-direction: row;
+     margin-bottom: 1em;
+}
+
+/* CCS FOR THE POST */
+.postContent {
+    background-color: brown;
+    background-size: 100%;
+    background-repeat: no-repeat;
+}
+/* ID for the anime man post image */
+#adachiPost {
+    background-image: url("../assets/adachi_true.jpg");
+    height: 383px;
+    width: 414px;
+}
+/* Profile CCS */ 
+.profilePic {
+    margin-left: 1em;
+    margin-right: 1em;
+    width: 50px;
+    height: 50px;
+    background-size: 100%;
+    background-color: blue;
+}
+
+#adachiPfp {
+    background-image: url("../assets/adachi_pfp.png");
+}
+
+.originalPoster {
+    margin-top: auto;
+    margin-bottom: auto;
+}
+/*Post Layout*/
+#PostLayout {
+
+    border: 2px solid;
+    border-radius: 25px;
+    padding: 1em 1em 1em 1em;
 }
 
 @media (min-width: 1024px) {
