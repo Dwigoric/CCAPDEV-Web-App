a0a535b4b6aafe12eec27709c254e684d456b87b
Joshua Permito <dwigoric@insanedevs.com>
2023-06-19 07:12:57 +0800

    feat: use preferred theme as initial theme

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -9,6 +9,10 @@ const toggleTheme = () => {
 
     document.documentElement.setAttribute('data-theme', !isDark ? 'dark' : 'light')
 }
+
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+document.documentElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light')
+isDarkStore.isDark = prefersDark
 </script>
 
 <template>
