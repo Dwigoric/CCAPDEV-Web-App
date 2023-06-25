363c153500a412e27e8ecdd8db9817c747f38af9
Thea1210 <althea_arceta@dlsu.edu.ph>
2023-06-25 16:48:49 +0800

    vote.js

--- src/components/PostSpecificVote.vue
+++ src/components/PostSpecificVote.vue
@@ -8,33 +8,42 @@ defineProps({
 })
 </script>
 
-<template>
-    <div class="dooters">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-            <path
-                fill="#888888"
-                d="M11 20V7.825l-5.6 5.6L4 12l8-8l8 8l-1.4 1.425l-5.6-5.6V20h-2Z"
-            />
-        </svg>
-        <div class="count">{{ reactions }}</div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-            <path fill="#888888" d="m12 20l-8-8l1.4-1.425l5.6 5.6V4h2v12.175l5.6-5.6L20 12l-8 8Z" />
-        </svg>
+<template id="post-template">
+    <div class>
+        <button @click="upvote" :class="{upvoted: upvoted}">&uarr;</button>
+        <span class="votes">{{ reactions }}</span>
+        <button @click="downvote" :class="{downvoted: downvoted}">&darr;</button>
     </div>
 </template>
 
 <style>
-.dooters {
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
+button {
+  &:hover {
+    cursor: pointer;
+  }
+  
+  &:focus {
+    outline: none;
+  }
 }
 
-.count {
-    margin: auto;
+.votes {
+  display: inline-block;
+  text-align: center;
+  width: 1.5em;
 }
 
-svg path:hover {
-    fill: orangered;
+.upvoted {
+  background-color: #FF8B60;
+  color: white;
+}
+
+.downvoted {
+  background-color: #9494FF;
+  color: white;
+}
+
+a {
+  margin-left: 0.75em;
 }
 </style>
