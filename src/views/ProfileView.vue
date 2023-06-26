ffa975ec87f0511388cc97e1441b6a7e649773d3
Joshua Permito <dwigoric@insanedevs.com>
2023-06-26 13:57:04 +0800

    fix(profile): import `useMediaQuery` + correct spelling of imported login store

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,24 +1,20 @@
 <script setup>
-
-import { useLoggedInStore } from '../stores/loggedIn';
+import { useLoggedInStore } from '../stores/loggedIn'
 import { useCachedPostsStore } from '@/stores/cachedPosts'
-import NavigationBar from '../components/NavigationBar.vue';
-import ThemeSwitch from '../components/ThemeSwitch.vue';
-import FeedPost  from '../components/FeedPost.vue';
+import { useMediaQuery } from '@vueuse/core'
+import NavigationBar from '../components/NavigationBar.vue'
+import ThemeSwitch from '../components/ThemeSwitch.vue'
+import FeedPost from '../components/FeedPost.vue'
+import NewPost from '../components/NewPost.vue'
 
-
-const login = useLoggedInStore();
+const login = useLoggedInStore()
 //const cachePosts = useCachedPostsStore();
-const navbar = NavigationBar;
-var {cachedPosts} = useCachedPostsStore();
-
-
-
-
+const navbar = NavigationBar
+var { cachedPosts } = useCachedPostsStore()
 
 defineProps({
     user: {
-        type: Object, 
+        type: Object,
         required: true
     },
 
@@ -26,35 +22,31 @@ defineProps({
         type: String,
         required: true
     }
-
 })
-
-
 </script>
 
 <template>
-    <navbar/>
+    <navbar />
     <div class="profile">
-        <div class = "prof-bg">
-            <img class="bg" src = "https://ik.imagekit.io/ikmedia/backlit.jpg"/>
+        <div class="prof-bg">
+            <img class="bg" src="https://ik.imagekit.io/ikmedia/backlit.jpg" />
         </div>
-        <div class = "prof-pfp">
-            <img class="pfp" :src=login.image :alt="`${[login.username]}'s pfp`"/>
+        <div class="prof-pfp">
+            <img class="pfp" :src="login.image" :alt="`${[login.username]}'s pfp`" />
         </div>
-        <div class = "username">
+        <div class="username">
             <h2>{{ login.username }}</h2>
         </div>
         <div class="description">
-            <h5>User hasn't placed a description of themselves yet.</h5> <!--default description, can be blank if they want-->
+            <h5>User hasn't placed a description of themselves yet.</h5>
+            <!--default description, can be blank if they want-->
         </div>
     </div>
 
-    <input type="button" value="Edit Profile" class="button"/>
+    <input type="button" value="Edit Profile" class="button" />
 
-    <hr class="solid"/> <!-- supposed to be the divider between profile details (username, pfp, etc.) and posts -->
-
-    
-
+    <hr class="solid" />
+    <!-- supposed to be the divider between profile details (username, pfp, etc.) and posts -->
 
     <div class="post" id="posts">
         <div
@@ -64,117 +56,112 @@ defineProps({
         >
             This is the left sidebar.
         </div>
-        <FeedPost 
-            v-for="posts in cachedPosts.filter(post => post.user.id === login.id)" 
+        <FeedPost
+            v-for="posts in cachedPosts.filter((post) => post.user.id === login.id)"
             :key="posts.id"
-            :id = "posts.id"
-            :title = "posts.title"
-            :body = "posts.body"
-            :user = "posts.user"
-            :image = "posts.image"
-            :reactions = "posts.reactions"/>
-            <div
+            :id="posts.id"
+            :title="posts.title"
+            :body="posts.body"
+            :user="posts.user"
+            :image="posts.image"
+            :reactions="posts.reactions"
+        />
+        <div
             class="feed-element"
             id="right-sidebar"
             v-if="useMediaQuery('(min-width: 1024px)').value"
         >
-            <NewPost v-if="loggedIn.username && cachedPosts.length > 0" :add-post="addPost" />
+            <NewPost v-if="login.username && cachedPosts.length > 0" :add-post="addPost" />
             <ThemeSwitch />
-        </div>    
-
+        </div>
     </div>
-
 </template>
 
 <style scoped>
-    .prof-bg{
-        position: absolute;
-        top: 48px;
-        width: 100%;
-        height: 50%
-    }
+.prof-bg {
+    position: absolute;
+    top: 48px;
+    width: 100%;
+    height: 50%;
+}
 
-    .bg{
-        width: 100vw;
-        height: 75%;
-        object-fit: cover;
-        z-index: 1;
-    }
+.bg {
+    width: 100vw;
+    height: 75%;
+    object-fit: cover;
+    z-index: 1;
+}
 
-    .prof-pfp{
-        position: relative;
-        top: 295px;
-        left: 5px;
-    }
+.prof-pfp {
+    position: relative;
+    top: 295px;
+    left: 5px;
+}
 
-    .pfp{
-        border-radius: 50%;
-        border: 5px solid;
-        border-color: black;
-        height: 125px;
-        width: 125px;
-        background-color: lightyellow;
-        z-index: 2;
-    }
+.pfp {
+    border-radius: 50%;
+    border: 5px solid;
+    border-color: black;
+    height: 125px;
+    width: 125px;
+    background-color: lightyellow;
+    z-index: 2;
+}
 
-    .username{
-        position: relative;
-        top: 195px;
-        left: 150px;
-        font-size: 25px;
-    }
+.username {
+    position: relative;
+    top: 195px;
+    left: 150px;
+    font-size: 25px;
+}
 
-    .description{
-        position: relative;
-        top: 198px;
-        left: 150px;
-        font-size: 19px;
-    }
+.description {
+    position: relative;
+    top: 198px;
+    left: 150px;
+    font-size: 19px;
+}
 
-    .button{
-        position: relative;
-        font-family: var(--source-sans);
-        font-size: 12px;
-        font-weight: 500;
-        color: var(--vt-c-black-soft);
-        background-color: var(--color-dark-pink);
-        border: none;
-        border-radius: 20px;
-        cursor: pointer;
-        top: 355px;
-        left: 825px;
-        height: 35px;
-        width: 100px;
-    }
+.button {
+    position: relative;
+    font-family: var(--source-sans);
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--vt-c-black-soft);
+    background-color: var(--color-dark-pink);
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    top: 355px;
+    left: 825px;
+    height: 35px;
+    width: 100px;
+}
 
-    .button:hover{
-        background-color: lightpink;
-        color: lightpink;
-    }
-    
+.button:hover {
+    background-color: lightpink;
+    color: lightpink;
+}
 
-    .solid{
-        height: 5px;
-        background: #6ad4d9;
-        bottom: 200px;
-    }
-    .post{
-        display: flex;
-        flex-flow: column nowrap;
-        padding: 2rem;
-        top: 460px;
-        right: 200px;
-    }
+.solid {
+    height: 5px;
+    background: #6ad4d9;
+    bottom: 200px;
+}
+.post {
+    display: flex;
+    flex-flow: column nowrap;
+    padding: 2rem;
+    top: 460px;
+    right: 200px;
+}
 
-    #posts {
-        flex-flow: column-reverse nowrap;
-        flex: 6;
-        justify-content: center;
-        align-items: center;
-        /*background-color: var(--color-background-mute);*/
-        gap: 2rem;
-    }
-
-
-    
+#posts {
+    flex-flow: column-reverse nowrap;
+    flex: 6;
+    justify-content: center;
+    align-items: center;
+    /*background-color: var(--color-background-mute);*/
+    gap: 2rem;
+}
 </style>
